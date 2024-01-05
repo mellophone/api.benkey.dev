858e2aab28fa66c215f8fa570546eb2d682a5850
Ben Key <54153434+mellophone@users.noreply.github.com>
2024-01-05 10:25:28 -0600

    Set broad access control headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://tasket.benkey.dev",
+          },
+          { key: "Access-Control-Expose-Headers", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, GET, OPTIONS, DELETE, PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "append,delete,entries,foreach,get,has,keys,set,values,Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
